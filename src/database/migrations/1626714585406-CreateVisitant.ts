00a2108950be942f58b406fb395d7e561e3ad33a
Harysson Soares <haryssonsoares@gmail.com>
2021-07-19 18:34:54 -0300

    feat: create visitante migration

---
+++ src/database/migrations/1626714585406-CreateVisitant.ts
@@ -0,0 +1,52 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateVisitant1626714585406 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'visitants',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'uuid',
+                    isPrimary: true
+                },
+                {
+                    name: 'cpf',
+                    type: 'varchar'
+                },
+                {
+                    name: 'last_login',
+                    type: 'timestamp',
+                    isNullable: true
+                },
+                {
+                    name: 'user_id',
+                    type: 'uuid',
+                },
+                {
+                    name: 'created_at',
+                    type: 'timestamp',
+                    default: 'now()'
+                },
+                {
+                    name: 'updated_at',
+                    type: 'timestamp',
+                    default: 'now()'
+                }
+            ],
+            foreignKeys: [
+                {
+                    columnNames: ['user_id'],
+                    referencedColumnNames: ['id'],
+                    referencedTableName: 'users'
+                }
+            ]
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('visitants')
+    }
+
+}
