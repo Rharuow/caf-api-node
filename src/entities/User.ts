b4e0297801d8c8b112057195feaa45b18f829441
Harysson Soares <haryssonsoares@gmail.com>
2021-07-20 00:58:34 -0300

    feat: user entity

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,9 +1,35 @@
-import { Entity, PrimaryColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, OneToMany, PrimaryColumn, UpdateDateColumn } from "typeorm";
 import { v4 as uuid } from 'uuid'
+import { Access } from "./Access";
 
 @Entity('users')
 export class User {
   @PrimaryColumn()
   readonly id: string
+
+  @Column()
+  username: string
+
+  @Column()
+  email: string
+
+  @Column()
+  password: string
+
+  @Column()
+  avatar: string
+
+  @CreateDateColumn()
+  created_at: Date
+
+  @UpdateDateColumn()
+  updated_at: Date
+
+  @OneToMany(() => Access, access => access.user)
+  access: Access
+
+  constructor() {
+    if(!this.id) this.id = uuid()
+  }
   
 }
