3828f6924b769481fafa6144bf8b0f311dabaaaa
Harysson Soares <haryssonsoares@gmail.com>
2021-07-20 18:12:54 -0300

    feat: create employee serive

---
+++ src/services/Employee/CreateService.ts
@@ -0,0 +1,29 @@
+
+import { getCustomRepository } from 'typeorm';
+import { EmployeeRepository } from '../../repositories/EmployeeRepository';
+
+interface IEmployee {
+  registration: string
+  user_id: string
+}
+
+class CreateService {
+  async execute({registration, user_id}: IEmployee){
+    const employeeRepository = getCustomRepository(EmployeeRepository)
+
+    try {
+      const employee = await employeeRepository.create({registration, user_id})
+
+      await employeeRepository.save(employee)
+
+      return employee
+    } catch (error) {
+      return error.message
+    }
+
+    
+
+  }
+}
+
+export default CreateService
