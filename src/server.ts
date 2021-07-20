d2d469509d6d4d7ab3562d51b6796f58c4c10f1b
Harysson Soares <haryssonsoares@gmail.com>
2021-07-20 18:07:55 -0300

    feat: middwares config routes

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import express from 'express'
+import cors from 'cors'
+import router from './routes';
 require('dotenv').config()
+
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
+app.use(router)
 
-app.listen(process.env.PORT || 3000, () => console.log(`start server at ${process.env.HOST}:${process.env.PORT}`))
+app.listen(process.env.PORT || 3000, () => {console.log(`start server at ${process.env.HOST}:${process.env.PORT}`)})
