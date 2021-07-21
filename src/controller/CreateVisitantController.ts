6aa657ecee2cfa0783f82250cb294223c555b57a
Harysson Soares <haryssonsoares@gmail.com>
2021-07-21 18:24:31 -0300

    feat: cloudinary file upload

--- src/controller/CreateVisitantController.ts
+++ src/controller/CreateVisitantController.ts
@@ -1,17 +1,18 @@
 import { Request, Response } from "express";
+import cloudinary from "../../cloudinary";
 import VisitantCreateService from '../services/Visitant/CreateService';
+
 
 
 export class CreateVisitantController {
   async handle(req: Request, res: Response) {
     const visitantCreateService = new VisitantCreateService()
 
-    console.log(req.file)
-    console.log(req.body)
+    const avatar = await cloudinary.upload(req.file) 
 
-    // const visitant = await visitantCreateService.execute({cpf: req.body.cpf, user: {email: req.body.email, username: req.body.username, avatar: req.body.avatar}})
+    const visitant = await visitantCreateService.execute({cpf: req.body.cpf, user: {email: req.body.email, username: req.body.username, avatar: avatar.url}})
 
-    
+    return res.json(visitant)
 
   }
 }
