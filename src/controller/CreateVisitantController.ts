afa15e63c940a8ed2feb80fbc2a1e564fbc6e407
Harysson Soares <haryssonsoares@gmail.com>
2021-07-26 18:11:07 -0300

    refact: status to create visitant controller

--- src/controller/CreateVisitantController.ts
+++ src/controller/CreateVisitantController.ts
@@ -7,18 +7,21 @@ export class CreateVisitantController {
     const visitantCreateService = new VisitantCreateService();
 
     // const avatar = await cloudinary.upload(req.file.buffer)
-
-    console.log(req.body);
-
-    const visitant = await visitantCreateService.execute({
-      cpf: req.body.cpf,
-      user: {
-        email: req.body.email,
-        username: req.body.username,
-        avatar: "avatar.url",
-      },
-    });
-
-    return res.json(visitant);
+    try {
+      const visitant = await visitantCreateService.execute({
+        cpf: req.body.cpf,
+        user: {
+          email: req.body.email,
+          username: req.body.username,
+          avatar: "https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper.png",
+          role: 'visitant'
+        },
+      });
+  
+      return res.json(visitant);
+      
+    } catch (error) {
+      return res.status(400).json({message: error.message})
+    }
   }
 }
