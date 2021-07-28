859d27012e2305cc523f1b04e7d90668cecdb9c2
Harysson Soares <haryssonsoares@gmail.com>
2021-07-28 17:45:55 -0300

    feat: routes to create employee, confirmation and session

--- src/routes.ts
+++ src/routes.ts
@@ -1,19 +1,35 @@
 import { Router } from "express";
 import multer from "multer";
+import { CreateEmployeeController } from "./controller/employee/CreateEmployeeController";
+import { CreateSessionController } from "./controller/session/CreateSessionController";
+import { ConfirmationSignupController } from "./controller/user/ConfirmationSignupController";
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-import { CreateVisitantController } from "./controller/CreateVisitantController";
+import { CreateVisitantController } from "./controller/visitant/CreateVisitantController";
 
 const router = Router();
 
 const createVisitantController = new CreateVisitantController();
+const createEmployeeCOntroller = new CreateEmployeeController();
+const confirmationSignupController = new ConfirmationSignupController();
+const createSessionController = new CreateSessionController();
 
 router.post(
   "/visitant",
   upload.single("photo"),
   createVisitantController.handle
 );
+
+router.post(
+  "/employee",
+  upload.single("photo"),
+  createEmployeeCOntroller.handle
+);
+
+router.post("/auth", createSessionController.handle);
+
+router.post("/confirmation", confirmationSignupController.handle);
 
 export default router;
