139845e95c1509912224f79956dc8563249ae3fa
Harysson Soares <haryssonsoares@gmail.com>
2021-07-28 17:49:17 -0300

    faet: create access service

--- src/services/Access/CreateService.ts
+++ src/services/Access/CreateService.ts
@@ -1,21 +1,33 @@
 import { getCustomRepository } from "typeorm";
 import { AccessRepository } from "../../repositories/AccessRepository";
 
-interface IAccess {
-  alphanumeric: string;
-  user_id: string;
-}
-
 class CreateService {
-  async execute({ alphanumeric, user_id }: IAccess) {
+  private generateCode(): string {
+    let result = "";
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const charactersLength = characters.length;
+    for (let i = 0; i < 8; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
+
+  async execute(user_id: string) {
     const accessRepository = getCustomRepository(AccessRepository);
     try {
-      const access = accessRepository.create({ alphanumeric, user_id });
+      const access = accessRepository.create({
+        alphanumeric: this.generateCode(),
+        user_id,
+      });
 
       await accessRepository.save(access);
 
+      console.log(access);
+
       return access;
     } catch (error) {
+      console.log(error);
       return error.message;
     }
   }
