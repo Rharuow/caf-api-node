4ac15390f8a6ce0dae464197954d23bd17809f5e
Harysson Soares <haryssonsoares@gmail.com>
2021-07-28 17:47:51 -0300

    chore: best praticies

--- src/server.ts
+++ src/server.ts
@@ -8,12 +8,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/v2', router);
+app.use("/v2", router);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`start server at ${process.env.HOST}:${process.env.PORT}`);
 });
-
 
 //TO-DO
 /*
