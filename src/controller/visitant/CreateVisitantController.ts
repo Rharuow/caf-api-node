e7bd50983dd278ea94bed54bf437ea8879b0419c
Harysson Soares <haryssonsoares@gmail.com>
2021-07-28 17:47:38 -0300

    reafct: path to create visitant controller

---
+++ src/controller/visitant/CreateVisitantController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import cloudinary from "../../../cloudinary";
+import VisitantCreateService from "../../services/Visitant/CreateService";
+
+export class CreateVisitantController {
+  async handle(req: Request, res: Response) {
+    const visitantCreateService = new VisitantCreateService();
+
+    const avatar = await cloudinary.upload(req.file.buffer);
+    try {
+      const visitant = await visitantCreateService.execute({
+        cpf: req.body.cpf,
+        user: {
+          email: req.body.email,
+          username: req.body.username,
+          avatar: avatar.url,
+          role: "visitant",
+        },
+      });
+
+      return res.json({ user: { email: visitant.user.email } });
+    } catch (error) {
+      return res.status(400).json({ message: error.message });
+    }
+  }
+}
