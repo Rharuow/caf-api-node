a3e6efcdbd42f2c0e5d7c89696dd0b3168264ad6
Harysson Soares <haryssonsoares@gmail.com>
2021-07-29 10:44:47 -0300

    feat: crpty password user

--- src/services/Session/CreateSessionService.ts
+++ src/services/Session/CreateSessionService.ts
@@ -1,25 +1,35 @@
 import { getCustomRepository } from "typeorm";
+import bcrypt from "bcryptjs";
+
 import { UserRepository } from "../../repositories/UserRepository";
+import { AccessRepository } from "../../repositories/AccessRepository";
 
 export class CreateSessionService {
   async execute(email: string, password: string) {
     const userRepository = getCustomRepository(UserRepository);
 
-    try {
-      const user = await userRepository.findOne({ where: { email, password } });
+    const accessRepository = getCustomRepository(AccessRepository);
 
-      const userAccess = user.accesses.find(
-        (access) => access.user_id === user.id
-      );
+    try {
+      const user = await userRepository.findOne({
+        where: { email },
+      });
+
+      const password_decoded = await bcrypt.compare(password, user.password);
+      if (!password_decoded) throw new Error("Invalid password");
+
+      const access = await accessRepository.findOne({
+        where: { user_id: user.id, is_active: true },
+      });
 
       return {
         username: user.username,
         photo: user.avatar,
         email: user.email,
-        code: userAccess.alphanumeric,
+        code: access.alphanumeric,
       };
     } catch (error) {
-      throw new Error("User's access not found");
+      throw new Error("Sorry we can't create a session!");
     }
   }
 }
