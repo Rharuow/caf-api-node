baae22f4def014eec60a1915ad311476d73b8f24
Harysson Soares <haryssonsoares@gmail.com>
2021-08-06 17:21:43 -0300

    chore: add procfile

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -3,12 +3,12 @@ require("dotenv").config();
 export default {
   url: process.env.DATABASE_URL,
   type: process.env.TYPEORM_CONNECTION,
-  // host: process.env.TYPEORM_HOST,
-  // port: process.env.TYPEORM_PORT,
-  // username: process.env.TYPEORM_USERNAME,
-  // password: process.env.TYPEORM_PASSWORD,
-  // database: process.env.TYPEORM_DATABASE,
-  // synchronize: process.env.TYPEORM_SYNCHRONIZE,
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+  synchronize: process.env.TYPEORM_SYNCHRONIZE,
   logging: false,
   cli: {
     migrationsDir: process.env.TYPEORM_MIGRATIONS_DIR,
