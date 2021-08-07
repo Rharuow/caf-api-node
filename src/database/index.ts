dbdccf5c021991c1179c56b8913f807b19d0899f
Harysson Soares <haryssonsoares@gmail.com>
2021-08-07 07:32:04 -0300

    try: ssl bug

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,12 +2,4 @@ import { createConnection } from "typeorm";
 
 require("dotenv").config()
 
-createConnection({
-    url: process.env.DATABASE_URL,
-    type: 'postgres',
-    // entities: process.env.TYPEORM_ENTITIES,
-    synchronize: true,
-    extra: {
-      ssl: true,
-    },
-  });
+createConnection();
