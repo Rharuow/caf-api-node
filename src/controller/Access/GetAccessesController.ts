e0fad933bfc4fdd229a4040be5305dd91645ff46
Harysson Soares <haryssonsoares@gmail.com>
2021-08-09 23:56:07 -0300

    refact: clean code get accesses controller

--- src/controller/Access/GetAccessesController.ts
+++ src/controller/Access/GetAccessesController.ts
@@ -1,13 +1,10 @@
 import { Request, Response } from "express";
-import { GetAccessesService } from "../../services/Access/GetAccessesService";
 import { GetCheckinsService } from "../../services/Checkin/GetCheckinsService";
 import { GetCheckoutsServices } from "../../services/Checkout/GetCheckoutsService";
 
 export class GetAccessesController {
   async handle(req: Request, res: Response) {
     const userID = req.headers.decodedSessionUserId as string;
-
-    const getAccessesService = new GetAccessesService();
 
     const getCheckinsService = new GetCheckinsService();
 
