9a8e49ee6bf534740123697e52c5759a14a7b03a
Harysson Soares <haryssonsoares@gmail.com>
2021-08-26 17:51:43 -0300

    refact: cluster logic to create service app express

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,8 @@
-import express from "express";
-import cors from "cors";
-import router from "./routes";
-import formData from "express-form-data";
-import os from "os";
-require("dotenv").config();
+import app from "./app";
+
 import "./database";
-
-// const options = {
-//   uploadDir: os.tmpdir(),
-//   autoClean: true,
-// };
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-// parse data with connect-multiparty.
-// app.use(formData.parse(options));
-// delete from the request all empty files (size == 0)
-app.use(formData.format());
-// change the file objects to fs.ReadStream
-app.use(formData.stream());
-// union the body and the files
-app.use(formData.union());
-app.use("/v2", router);
+require("dotenv").config();
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`start server at`);
+  console.log(`start server at ${process.env.HOST}:${process.env.PORT}/v2`);
 });
