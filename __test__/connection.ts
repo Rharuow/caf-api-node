4cc2d2ccbb06afcda1d17ce0ef9f5021527deec8
Harysson Soares <haryssonsoares@gmail.com>
2021-08-31 07:56:46 -0300

    wip: tests connection

--- __test__/connection.ts
+++ __test__/connection.ts
@@ -5,47 +5,38 @@ require("dotenv").config({
 
 const connection = {
   async create() {
-    await createConnection({
-      name: "test",
-      type: "postgres",
-      host: "localhost",
-      port: 5432,
-      username: "postgres",
-      password: "",
-      database: "caf_api_node_test",
-      cli: {
-        migrationsDir: process.env.TYPEORM_MIGRATIONS_DIR,
-        entitiesDir: process.env.TYPEORM_ENTITIES_DIR,
-      },
-      entities: [process.env.TYPEORM_ENTITIES],
-      migrations: [process.env.TYPEORM_MIGRATIONS],
-    });
+    await createConnection();
+    await getConnection().runMigrations()
   },
 
   async close() {
-    await getConnection("test").close();
+    await getConnection().close();
   },
 
   async clear() {
-    const connection = getConnection("test");
-    console.log(getConnection("test"))
+    const connection = getConnection();
     const entities = connection.entityMetadatas;
 
     entities.forEach(async (entity) => {
       const repository = connection.getRepository(entity.name);
+
       await repository.query(`DELETE FROM ${entity.tableName}`);
     });
   },
 };
 
 beforeAll(async () => {
+  console.log("CREATE")
   await connection.create();
 });
 
-afterAll(async () => {
-  await connection.close();
-});
 
 beforeEach(async () => {
+  console.log("CLEAR")
   await connection.clear();
+});
+
+afterAll(async () => {
+  console.log("CLOSE")
+  await connection.close().catch(err => console.log(err));
 });
