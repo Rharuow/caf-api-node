cb7c81602ede36a67d381a8adc8c83787da79a0c
Harysson Soares <haryssonsoares@gmail.com>
2021-08-30 22:23:53 -0300

    wip: tests connection

--- __test__/connection.ts
+++ __test__/connection.ts
@@ -1,4 +1,42 @@
-import connection from "../src/database";
+import { createConnection, getConnection } from "typeorm";
+require("dotenv").config({
+  path: ".env.test",
+})
+
+const connection = {
+  async create() {
+    await createConnection({
+      name: "test",
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "postgres",
+      password: "",
+      database: "caf_api_node_test",
+      cli: {
+        migrationsDir: process.env.TYPEORM_MIGRATIONS_DIR,
+        entitiesDir: process.env.TYPEORM_ENTITIES_DIR,
+      },
+      entities: [process.env.TYPEORM_ENTITIES],
+      migrations: [process.env.TYPEORM_MIGRATIONS],
+    });
+  },
+
+  async close() {
+    await getConnection("test").close();
+  },
+
+  async clear() {
+    const connection = getConnection("test");
+    console.log(getConnection("test"))
+    const entities = connection.entityMetadatas;
+
+    entities.forEach(async (entity) => {
+      const repository = connection.getRepository(entity.name);
+      await repository.query(`DELETE FROM ${entity.tableName}`);
+    });
+  },
+};
 
 beforeAll(async () => {
   await connection.create();
