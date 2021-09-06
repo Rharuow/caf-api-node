370650969acdf94d93c9b6a8f47965e3dcc5d5ab
Harysson Soares <haryssonsoares@gmail.com>
2021-09-05 23:37:59 -0300

    wip: session create

--- __test__/connection.ts
+++ __test__/connection.ts
@@ -1,6 +1,8 @@
 import connection from "../src/database";
 import request from "supertest";
 import app from "../src/app";
+import { getCustomRepository } from "typeorm";
+import { UserRepository } from "../src/repositories/UserRepository";
 
 beforeAll(async () => {
   await connection.create();
@@ -10,9 +12,8 @@ afterAll(async () => {
   await request(app).delete("/v2/user").send({ email: "naxacif239@mnqlm.com" });
   await request(app).delete("/v2/user").send({ email: "joben44053@mom2kid.com" });
 
+  const userRepository = getCustomRepository(UserRepository)
+
+  await userRepository.delete({email: "test@mail.com"})
   await connection.close();
 });
-
-// beforeEach(async () => {
-//   await connection.clear();
-// });
--- __test__/session.test.ts
+++ __test__/session.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest'
 import { getCustomRepository } from 'typeorm'
+import app from '../src/app'
 import { UserRepository } from '../src/repositories/UserRepository'
 
 import "./connection"
@@ -8,7 +9,16 @@ describe("Tests to create a session with jwt", () => {
   test("Should return jwt with correctly credentials", async () => {
     const userRepository = getCustomRepository(UserRepository)
 
-    const user = await userRepository.find()
+    const user = userRepository.create({
+      email: 'test@mail.com',
+      password: '123123123',
+      username: 'test',
+      role: 'visitant',
+      avatar: 'https://www.nerdssauros.com.br/wp-content/uploads/2021/02/avatar.jpeg',
+      confirmation_token: '3216548979846213'
+    })
+
+    await userRepository.save(user)
 
     console.log("USERS = ", user)
   })
