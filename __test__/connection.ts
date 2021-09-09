d1eb76cdbfb7eeaf04e0beeb7930477dc041b813
Harysson Soares <haryssonsoares@gmail.com>
2021-09-08 21:41:56 -0300

    deploy: pipeline test

--- __test__/connection.ts
+++ __test__/connection.ts
@@ -9,7 +9,7 @@ beforeAll(async () => {
 });
 
 afterAll(async () => {
-  await request(app).delete("/v2/user").send({ email: "naxacif239@mnqlm.com" });
+  await request(app).delete("/v2/user").send({ email: "pebeli9111@silbarts.com" });
   await request(app)
     .delete("/v2/user")
     .send({ email: "joben44053@mom2kid.com" });
