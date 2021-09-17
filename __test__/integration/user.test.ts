6f994a348f2a6fc653ff70b20d1e2020be881653
Harysson Soares <haryssonsoares@gmail.com>
2021-09-17 11:42:38 -0300

    feat: created new test to user already resgistred case

--- __test__/integration/user.test.ts
+++ __test__/integration/user.test.ts
@@ -44,6 +44,22 @@ describe("Test about users context", () => {
     expect(response.body.text).toBe("user created with success");
   });
 
+  test("should return text 'user created with success' when confirmation password has done with success", async () => {
+    const userRepository = getCustomRepository(UserRepository);
+
+    const user = await userRepository.findOneOrFail({
+      where: { email: "pebeli9111@silbarts.com" },
+    });
+
+    const response = await request(app).post("/v2/confirmation").send({
+      token: user.confirmation_token,
+      role: user.role,
+      password: "123123123",
+    });
+
+    expect(response.body.text).toBe("user created with success");
+  });
+
   test("should return status 200 when create employee done", async () => {
     const response = await request(app)
       .post("/v2/employee")
@@ -72,5 +88,21 @@ describe("Test about users context", () => {
     });
 
     expect(response.body.text).toBe("user created with success");
+  });
+
+  test("should return status 400 when create registrated employee had done", async () => {
+    const response = await request(app)
+      .post("/v2/employee")
+      .set("Accept", "application/json")
+      .set("Content-Type", "multipart/form-data")
+      .set("connection", "keep-alive")
+      .field("username", "Test Employee")
+      .field("email", "joben44053@mom2kid.com")
+      .field("registration", "123123123123")
+      .attach("photo", "__test__/images/avatar.png");
+
+    console.log(response.body);
+
+    expect(response.status).toBe(400);
   });
 });
